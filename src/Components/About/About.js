75ad7a034a05b570d8205daf32be721f3115dd68
Pobarul-islam <pobarul3@gmail.com>
2022-09-02 19:06:30 +0600

    about section added

---
+++ src/Components/About/About.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+             <div class="hero min-h-screen">
+  <div className="hero-content flex-col lg:flex-row mr-10">
+    <img src="https://i.ibb.co/gRLY9FW/ima-removebg-preview.png" alt='' class="max-w-sm rounded-lg" />
+    <div className='ml-20'>
+
+    <p>
+    I am a very good Learner
+    I can learn a new software or other subject very <br /> quickly
+    My Communication Skill is not so poor.I can communicate with <br /> any type of person
+    My strengths are I am positive, passionate, a quick learner, <br /> and a hardworking person.
+    The main fact is, I love MYSELFI am a very good <br /> Learner
+    I can learn a new software or other subject very quickly
+    My Communication <br /> Skill is not so poor.I can communicate with any type of person
+    My strengths are I am <br /> positive, passionate, a quick learner, and a hardworking person. <br />
+    The main fact is, I love MYSELF
+</p>
+      <button className="btn btn-primary mt-8">Download resume</button>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default About;
+
+
+
+
+
