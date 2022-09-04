e25b776ecd2120f6c7c510520902b3b600225403
Pobarul-islam <pobarul3@gmail.com>
2022-09-04 09:41:48 +0600

    responsived

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -15,7 +15,7 @@ const Projects = () => {
     return (
         <div className='mt-20'>
             <h2 className='text-4xl text-center font-bold mt-10'>Projects</h2>
-              <div className='grid grid-cols-3 ml-10 gap-y-10 mt-10'>
+              <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4 ml-10 gap-y-10 mt-10'>
            
             {
                 projects.map(project => <SingleProject
