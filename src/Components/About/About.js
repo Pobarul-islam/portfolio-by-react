2e46dcab5d1a00e508efada0f16338d8f0c8b1d9
Pobarul-islam <pobarul3@gmail.com>
2022-09-13 19:45:00 +0600

    updated

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,33 +2,40 @@ import React from 'react';
 import './About.css'
 const About = () => {
     return (
-        <div>
-            <h2 className=' text-4xl text-center font-bold text-white'>About</h2>
-            <div class="min-h-screen hero">
-                
-  <div className="hero-content flex-col lg:flex-row mr-10">
-    <img src="https://i.ibb.co/gRLY9FW/ima-removebg-preview.png" alt=''/>
-    <div className='aboutText text-blue-100 p-3'>
-
-    <p>
-    My name is Pobarul islam, and I am from Thakurgaon, Bangladesh. <br />
-
-Along with my study, I have completed Front end web development course in programming hero and also self-improvement some skills like React, React redux, typescript, socket.io, Node, Express, MongoDB and Mongoose, etc.
-
-My strengths are I am positive, passionate, a quick learner, and a hardworking person.
-
-My short-term goal is to get a job or intern in a reputed company where I can grow my working skills and gain more and more knowledge that will help me in the future.
-
-My long-term goal is to become a more responsible and knowledgeable personality and achieve a good position in the organization.
-
-That's all about me. And thanks again for this great opportunity.
-
-</p>
-     <a className='btn' href="https://drive.google.com/file/d/1TlC8Yheazw1CwHZ3fI309kP9GLGyMTma/view?usp=sharing">Download Resume</a>
-    </div>
-  </div>
-</div>
+      <div>
+        <h2 className=" text-4xl text-center font-bold text-white">About</h2>
+        <div class="min-h-screen hero">
+          <div className="hero-content flex-col lg:flex-row mr-10">
+            <img
+              src="https://i.ibb.co/gRLY9FW/ima-removebg-preview.png"
+              alt=""
+            />
+            <div className="aboutText text-blue-100 p-3">
+              <p>
+                My name is Pobarul islam, and I am from Thakurgaon, Bangladesh.{" "}
+                <br />
+                Along with my study, I have completed Front end web development
+                course in programming hero and also self-improvement some skills
+                like React, React redux, typescript, socket.io, Node, Express,
+                MongoDB and Mongoose, etc. My strengths are I am positive,
+                passionate, a quick learner, and a hardworking person. My
+                short-term goal is to get a job or intern in a reputed company
+                where I can grow my working skills and gain more and more
+                knowledge that will help me in the future. My long-term goal is
+                to become a more responsible and knowledgeable personality and
+                achieve a good position in the organization. That's all about
+                me. And thanks again for this great opportunity.
+              </p>
+              <a
+                className="btn"
+                href="https://drive.google.com/file/d/1FPpCN18pHTBQt0EUShrXDVfkT4jSYxjo/view?usp=sharing"
+              >
+                Download Resume
+              </a>
+            </div>
+          </div>
         </div>
+      </div>
     );
 };
 
